6f9d6df6f0dd0f21979e62efc5c4b19a7a9edf18
Vilo4ka <saiviolet@gmail.com>
2022-02-13 20:51:56 +0300

    поиск по рф. без стилей

--- src/App.js
+++ src/App.js
@@ -1,31 +1,87 @@
 import './App.css';
+import React, {useState} from 'react';
+const API_KEY = '3e414d62e193348a63663b62133b945d';
+const API_URL = 'http://api.openweathermap.org/data/2.5/'
 
-const API_KEY = '9f9d777d-be85-4c8f-ae74-7fc9319f33d1';
-const API_URL = 'https://api.weather.yandex.ru/v2/informers?';
-
-let latitude, longitude;
-navigator.geolocation.getCurrentPosition(position => {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-}, error => {
-  console.error(error)
-})
+// let latitude, longitude;
+// navigator.geolocation.getCurrentPosition(position => {
+//   latitude = position.coords.latitude;
+//   longitude = position.coords.longitude;
+// }, error => {
+//   console.error(error)
+// })
 
 function App() {
-   const getWeather = () => {
-    return fetch(`${API_URL}lat=${latitude}&lon=${longitude}`, {
-      headers: {
-        'X-Yandex-API-Key': API_KEY 
-      }
-    })
-      .then(res => console.log (res));
+  const [value, setValue] = useState('');
+  const [weather, setWeather] = useState('');
+  const [city, setCity] = useState('');
+  let weatherHtml;
+
+   const getWeather = (evt) => {
+
+    if (evt.key === 'Enter') {
+      fetch(`${API_URL}/find?q=${value}&type=like&APPID=${API_KEY}&lang=ru&units=metric`)
+        .then(res => res.json())
+        //для городов рф
+        .then(res => {
+          const result = res.list.find(city => {return city.sys.country === 'RU'});
+          setCity(value.toUpperCase());
+          setWeather(result);
+          setValue('');
+          console.log(result);
+        })
+        .catch(err => {console.log(err)})
+    }
   }
 
-
-
+  if (weather === '') weatherHtml = '';
+  else if (weather === undefined) weatherHtml = (<div className="weather">
+    <div className="country">
+      <h1>Ошибка</h1>
+      <h3>Возможно такой город еще не построили или вы ошиблись при вводе названия</h3>
+    </div>
+    </div>)
+  else weatherHtml = (<div className="weather">
+    <h1 className="country">{city}</h1>
+    <div className="temp">{Math.floor(weather.main.temp)}&deg;C</div>
+    <h3 className="feels_like">по ощущениям {Math.floor(weather.main.feels_like)}&deg;С</h3>
+    <h2 className="description">{weather.weather[0].description}</h2>
+  </div>)
+  let classNameApp;
+  //TODO заменить на свитч
+  if (weather === '' || weather === undefined) classNameApp = 'App';
+  else switch (weather.weather[0].main) {
+    case ('Clouds' || 'Drizzle'):
+      classNameApp = 'App app__cloud'
+      break;
+    case 'Clear':
+      classNameApp = 'App app__clear'
+      break;  
+    case 'Thunderstorm':
+      classNameApp = 'App app__storm'
+      break; 
+    case 'Rain':
+      classNameApp = 'App app__rain'
+      break;   
+    case 'Snow':
+      classNameApp = 'App app__snow'
+      break; 
+  }
   return (
-    <div className="App">
-     <button onClick={getWeather}>click</button>
+    <div className={classNameApp}>
+      <main className = "container">
+        <input 
+        className= "input"
+        type = "text"
+        placeholder = "Введите город и нажмите Enter..."
+        value = {value}
+        onChange = {evt => {
+          setValue(evt.target.value)
+        }}
+        onKeyPress = {getWeather}
+        />
+        {weatherHtml}
+      </main>
     </div>
   );
 }
